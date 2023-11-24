b3be5de902c8ceae2450db80be974628b97173ee
Mabouya04 <iilbarcelona9@gmail.com>
2023-11-24 20:17:14 +0800

    Edit script

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Perintah untuk build Docker image
-docker build -t mabouya/karsajobs:latest .
+docker build -t mabouya/karsajobs-ui:latest .
 
 # Perintah untuk login ke Docker Hub
 docker login -u mabouya -p Mabouya00004
 
 # Perintah untuk push image ke Docker Hub
-docker push mabouya/karsajobs:latest
+docker push mabouya/karsajobs-ui:latest
